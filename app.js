87c8c38c0edfa7987074bfe358c8aba286469fb6
Enkhbayr <enkhbayr049@gmail.com>
2023-12-03 00:19:03 +0800

    tusuviig delgetsend update hiine

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var uiController = (function () {
     addBtn: ".add__btn",
     incomeList: ".income__list",
     expenseList: ".expenses__list",
+    tusuvLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expneseLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage",
   };
   return {
     getInput: function () {
@@ -31,6 +35,21 @@ var uiController = (function () {
         fieldsArr[i].value = "";
       }
       fieldsArr[0].focus();
+    },
+
+    tusuviigUzuuleh: function (tusuv) {
+      document.querySelector(DOMstrings.tusuvLabel).textContent = tusuv.tusuv;
+      document.querySelector(DOMstrings.incomeLabel).textContent =
+        tusuv.totalInc;
+      document.querySelector(DOMstrings.expneseLabel).textContent =
+        tusuv.totalExp;
+      if (tusuv.huvi !== 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuv.huvi + "%";
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuv.huvi;
+      }
     },
 
     addListItem: function (item, type) {
@@ -104,8 +123,8 @@ var financeController = (function () {
       return {
         tusuv: data.tusuv,
         huvi: data.huvi,
-        totalinc: data.totals.inc,
-        totalexp: data.totals.exp,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
       };
     },
 
@@ -153,6 +172,7 @@ var appController = (function (uiController, financeController) {
       financeController.tusuvTootsoloh();
       var tusuv = financeController.tusuviigAvah();
       console.log(tusuv);
+      uiController.tusuviigUzuuleh(tusuv);
       //4. төсөвийг тооцоолно.
       //5. эцэсийн үлдэгдэл тооцоог дэлгэцэд гаргана.
     }
@@ -170,7 +190,14 @@ var appController = (function (uiController, financeController) {
   };
   return {
     init: function () {
-      return setUpEventListeners();
+      console.log("app started...");
+      uiController.tusuviigUzuuleh({
+        tusuv: 0,   
+        huvi: 0,
+        totalInc: 0,
+        totalExp: 0,
+      });
+      setUpEventListeners();
     },
   };
 })(uiController, financeController);
