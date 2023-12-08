481d6af9d296cdfe78575588d92c5673e790bd93
Enkhbayr <enkhbayr049@gmail.com>
2023-12-08 18:07:36 +0800

    ustgah uildel delgetsend garna

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var uiController = (function () {
     incomeLabel: ".budget__income--value",
     expneseLabel: ".budget__expenses--value",
     percentageLabel: ".budget__expenses--percentage",
-    clrBtn: ".item__delete--btn",
+    containerDiv: ".container",
   };
   return {
     getInput: function () {
@@ -52,17 +52,21 @@ var uiController = (function () {
           tusuv.huvi;
       }
     },
+    deleteListItem: function (id) {
+      var el = document.getElementById(id);
+      el.parentNode.removeChild(el);
+    },
 
     addListItem: function (item, type) {
       var html, list;
       if (type === "inc") {
         list = DOMstrings.incomeList;
         html =
-          '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
+          '<div class="item clearfix" id="inc-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
       } else {
         list = DOMstrings.expenseList;
         html =
-          '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
+          '<div class="item clearfix" id="exp-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
       // Ð¢ÑÑ€ HTML Ð´Ð¾Ñ‚Ñ€Ð¾Ð¾ Ð¾Ñ€Ð»Ð¾Ð³Ð¾ Ð·Ð°Ñ€Ð»Ð°Ð³Ñ‹Ð½ ÑƒÑ‚Ð³ÑƒÑƒÐ´Ñ‹Ð³ REPLACE Ð°ÑˆÐ¸Ð³Ð»Ð°Ð¶ Ó©Ó©Ñ€Ñ‡Ð¸Ð»Ð¶
       html = html.replace("%id%", item.id);
@@ -183,7 +187,7 @@ var appController = (function (uiController, financeController) {
       uiController.clearFields();
       financeController.tusuvTootsoloh();
       var tusuv = financeController.tusuviigAvah();
-      console.log(tusuv);
+      // console.log(tusuv);
       uiController.tusuviigUzuuleh(tusuv);
       //4. төсөвийг тооцоолно.
       //5. эцэсийн үлдэгдэл тооцоог дэлгэцэд гаргана.
@@ -199,6 +203,22 @@ var appController = (function (uiController, financeController) {
         ctrlAddItem();
       }
     });
+    document
+      .querySelector(DOM.containerDiv)
+      .addEventListener("click", function (event) {
+        var id = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+        if (id) {
+          var arr = id.split("-");
+          var type = arr[0];
+          var itemId = parseInt(arr[1]);
+          console.log(type + "-" + itemId);
+
+          financeController.deleteItem(type, itemId);
+
+          uiController.deleteListItem(id);
+        }
+      });
   };
   return {
     init: function () {
